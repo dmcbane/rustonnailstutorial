5b5e56f64a8cf6b3200396bb420072a7a6bbaf30
H. Dale McBane <h.dale.mcbane@gmail.com>
2024-07-18 17:51:10 +0000

    Pico CSS for quick, simple layout without minimal changes

--- crates/web-pages/src/layout.rs
+++ crates/web-pages/src/layout.rs
@@ -20,6 +20,10 @@ pub fn Layout(title: String, children: Element) -> Element {
                 name: "viewport",
                 content: "width=device-width, initial-scale=1"
             }
+            link {
+                rel: "stylesheet",
+                href: "https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.classless.min.css"
+            }
         }
         body {
             {children}
